8bb1082a3c2fd03e1025248966e60c2d8fd11e95
AlexandreGlatz <alexandre.glatzpro@gmail.com>
2026-01-15 23:28:52 +0100

    [ADD] Templates

---
+++ src/res/templates/include_template.h
@@ -0,0 +1,10 @@
+#ifndef _<NAME_INCLUDE>_H__
+#define _<NAME_INCLUDE>_H__
+
+class <NAME> {
+public:
+    <NAME>();
+    ~<NAME>();
+
+}
+#endif
---
+++ src/res/templates/main_template.cpp
@@ -0,0 +1,10 @@
+////////////////////////////////////////////////////
+// CMake Generator by Alexandre Glatz
+// https://github.com/AlexandreGlatz/cmake-generator
+////////////////////////////////////////////////////
+#include <iostream>
+
+int main()
+{
+    std::cout<<"Hello World !"<<std::endl;
+}
