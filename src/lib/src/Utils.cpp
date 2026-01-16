d252bd8d5c97f7783c33c72b4e6d003c8da28826
AlexandreGlatz <alexandre.glatzpro@gmail.com>
2026-01-16 10:17:48 +0100

    [ADD] Utilities

--- src/lib/src/Utils.cpp
+++ src/lib/src/Utils.cpp
@@ -4,9 +4,9 @@
 std::string Utils::StringReplace(std::string base, std::string tag, std::string replacement)
 {
     if(base.find(tag) == std::string::npos)
-        return "";
+        return base;
 
-    base.replace(base.find(tag), base.length(), replacement);
+    base.replace(base.find(tag), tag.length(), replacement);
     
     return base;
 }
@@ -34,6 +34,25 @@ std::string Utils::GetTemplate(FolderManager::FILE_CONTENT templateType)
             return Utils::Parse("../res/templates/CMakeApp_template.txt");
         case FolderManager::FILE_CONTENT::CMAKE_GEN:
             return Utils::Parse("../res/templates/CMakeGen_template.txt");
-
+        case FolderManager::FILE_CONTENT::CMAKE_LIB:
+            return Utils::Parse("../res/templates/CMakeLib_template.txt");
+        case FolderManager::FILE_CONTENT::INCLUDE:
+            return Utils::Parse("../res/templates/include_template.h");
+        case FolderManager::FILE_CONTENT::MAIN:
+            return Utils::Parse("../res/templates/main_template.cpp");
+        case FolderManager::FILE_CONTENT::SRC:
+            return Utils::Parse("../res/templates/main_template.cpp");
     }
+    return "";
+}
+
+std::string Utils::ToUpper(std::string const& str)
+{
+    std::string out;
+    for(int i = 0; i< str.size(); ++i)
+    {
+        out[i] = toupper(str[i]);
+    }
+
+    return out;
 }
