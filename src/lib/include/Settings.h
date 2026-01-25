9bd631cad3f0143983fbada2d1254dfbd23eed5a
AlexandreGlatz <alexandre.glatzpro@gmail.com>
2026-01-26 00:35:36 +0100

    [ADD] Parsing

--- src/lib/include/Settings.h
+++ src/lib/include/Settings.h
@@ -3,17 +3,27 @@
 
 #include <vector>
 #include <map>
+#include <string>
 class Settings {
 public:
     Settings(int argc, char** argv);
     ~Settings();
     
-    std::map<char*, std::vector<char*>> Parse();
+    void Parse();
+
+    std::map<const char*, std::vector<const char*>> GetProjectDependencies() { return m_projectDependencies; }
+    const char* GetExecutable() { return m_executable; }
+    const char* GetRootName() { return m_rootName; }
 
 private:
-    std::vector<char*> _GetDependencies(int argc, char** argv, int index);
+    std::vector<const char*> _GetDependencies(int argc, char** argv, int index);
     int m_argc;
     char** m_argv;
+
+private:
+    std::map<const char*, std::vector<const char*>> m_projectDependencies;
+    const char* m_executable;
+    const char* m_rootName;
 
 };
 #endif
--- src/lib/src/Settings.cpp
+++ src/lib/src/Settings.cpp
@@ -1,44 +1,69 @@
 #include "Settings.h"
 #include <string.h>
-
+#include <iostream>
 Settings::Settings(int argc, char** argv):
     m_argc(argc),
-    m_argv(argv)
+    m_argv(argv),
+    m_projectDependencies()
 {
 
 }
 
-std::map<char*, std::vector<char*>> Settings::Parse()
+Settings::~Settings()
 {
-    std::map<char*, std::vector<char*>> result; 
-    int index = 2;
-    for(int i = 0; i<m_argc; ++i)
+
+}
+
+void Settings::Parse()
+{
+    m_executable = m_argv[0];
+    if(m_argc == 1)
     {
-        if(strcmp(m_argv[i], "-r") == 0 || strcmp(m_argv[i], "--root") == 0)
-        {
-            result[m_argv[i + 1]] = _GetDependencies(m_argc, m_argv, index);
-        }
-
-        if(strcmp(m_argv[i], "-a") == 0 || strcmp(m_argv[i], "--app") == 0)
-        {
-            result[m_argv[i + 1]] = _GetDependencies(m_argc, m_argv, index);
-        }
-
-        if(strcmp(m_argv[i], "-l") == 0 || strcmp(m_argv[i], "--lib") == 0)
-        {
-            result[m_argv[i + 1]] = _GetDependencies(m_argc, m_argv, index);
-        }
+        m_projectDependencies["app"] = {"lib"}; 
+        m_projectDependencies["lib"] = {}; 
+        m_rootName = "project";
+        return;
     }
 
-    return result;
+    for(int i = 1; i<m_argc; ++i)
+    {
+        std::cout<<i<<std::endl;
+        if(strcmp(m_argv[i], "-r") == 0 || strcmp(m_argv[i], "--root") == 0)
+        {
+            m_rootName = m_argv[i+1];
+            i++;
+        }
+
+        else if(strcmp(m_argv[i], "-a") == 0 || strcmp(m_argv[i], "--app") == 0)
+        {
+            std::vector<const char*> dependencies = _GetDependencies(m_argc, m_argv, i);
+            m_projectDependencies[m_argv[i + 1]] = dependencies;
+            i += dependencies.size() + 1;
+        }
+        else if(strcmp(m_argv[i], "-l") == 0 || strcmp(m_argv[i], "--lib") == 0)
+        {
+            std::vector<const char*> dependencies = _GetDependencies(m_argc, m_argv, i);
+            m_projectDependencies[m_argv[i + 1]] = dependencies;
+            i += dependencies.size() + 1;
+        }
+        else
+        {
+            std::cout<<"command : "<<m_argv[i]<<" does not exist, use -h for help"<<std::endl;
+        }
+    }
 }
 
-std::vector<char*> Settings::_GetDependencies(int argc, char** argv, int index)
+std::vector<const char*> Settings::_GetDependencies(int argc, char** argv, int index)
 {
-    std::vector<char*> dependencies;
+    
+    std::vector<const char*> dependencies;
     int argument = 2;
-    while(argv[index + argument][0] != '-' || index + argument >= argc)
+    if(index + argument >= argc)
+        return dependencies;
+
+    while(argv[index + argument][0] != '-' && index + argument < argc)
     {
+        std::cout<<argv[index + argument]<<std::endl;
         dependencies.push_back(argv[index + argument]);
         argument++;
     }
